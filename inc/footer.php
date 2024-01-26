cc6741d663f2ff548c358b1078ed81c9b16d6644
NhatAnh7 <156416963+NhatAnh7@users.noreply.github.com>
2024-01-26 16:51:56 +0700

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span> BAN QUYEN BOI NHAT ANH
+        
+      </p>
+    </div>
+  </footer>
